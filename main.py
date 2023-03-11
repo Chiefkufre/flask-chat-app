0d3c5680f01c9b17a47d0cd32c17611a6a4f9b7b
chiefkufre <samuelkufrewillie@gmail.com>
2023-03-11 22:16:43 +0100

    chore: add code to process post request params and unique code generator function

--- main.py
+++ main.py
@@ -10,11 +10,43 @@ app.config['SECRET_KEY'] = "HDSGHGDHSGHSD"
 socketio = SocketIO(app)
 
 
+rooms = {}
+
+def generate_unique_code(length):
+    while True:
+        code = ""
+        for _ in range(length):
+            code += random.choice(ascii_uppercase)
+        
+        if code not in rooms:
+            break
+
+    return code
+        
+
 
 # home route definition
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        code = request.form.get("code")
+        join = request.form.get('join', False)
+        create = request.form.get('create', False)
+
+
+        if not name:
+            return render_template("home.html", error="please enter a name")
+
+        if join != False and not code:
+            return render_template("home.html", error="please enter a room code")
+
+
+        room = code
+        if create != False:
+            room = generate_unique_code(4)
+
     return render_template("home.html")
 
 if __name__ == "__main__":
